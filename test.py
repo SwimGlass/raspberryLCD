698b4afdd0f81ab9f1d94eff744dfdee841f5eca
SwimGlass <swimmingglasses@gmail.com>
2016-01-26 16:04:18 +0800

    modify test.py to print rectangle

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ screen = pygame.display.set_mode(size)
 
 while True:
     for event in pygame.event.get():
-        if event.type() == pygame.QUIT:
+        if event.type == pygame.QUIT:
             sys.exit()
 
-line_color=(100,0,0)
+color=(100,0,0)
 
-pygame.draw.line(screen,line_color,(50,50),(200,200),5)
+pygame.draw.rect(screen,color,(50,50,200,200)
 
 pygame.display.flip()
