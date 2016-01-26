ea3ed9255f13377b87cc729284341f4bcbb1da27
SwimGlass <swimmingglasses@gmail.com>
2016-01-26 16:50:14 +0800

    add test1.py to test bouncing rectangle

---
+++ test1.py
@@ -0,0 +1,35 @@
+import pygame
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+pygame.init()
+size = [400, 300]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Bouncing Rectangle")
+done = False
+clock = pygame.time.Clock()
+rect_x = 50
+rect_y = 50
+
+rect_change_x = 2
+rect_change_y = 2
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    rect_x += rect_change_x
+    rect_y += rect_change_y
+
+    if rect_y > 250 or rect_y < 0:
+        rect_change_y = rect_change_y * -1
+    if rect_x > 350 or rect_x < 0:
+        rect_change_x = rect_change_x * -1
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
+    pygame.draw.rect(screen, RED, [rect_x + 10, rect_y + 10, 30, 30])
+    clock.tick(60)
+    pygame.display.flip()
+pygame.quit()
